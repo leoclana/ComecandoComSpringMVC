92106c7a7b9cab178bac447ba5d38f18a02cb842
Leonardo Lana <leoclana@gmail.com>
2020-07-16 18:44:10 -0300

    2.7. Conectando ao MySQL - Retorno SpringBoot 1.3.1
    Erro no Timezone com Spring 2, retornei para o SpringBoot 1.3.1

--- cobranca/src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ cobranca/src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -26,7 +26,7 @@ public class Titulo {
 	
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
-	private Date dataVencimeto;
+	private Date dataVencimento;
 	
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
@@ -50,12 +50,12 @@ public class Titulo {
 		this.descricao = descricao;
 	}
 
-	public Date getDataVencimeto() {
-		return dataVencimeto;
+	public Date getDataVencimento() {
+		return dataVencimento;
 	}
 
-	public void setDataVencimeto(Date dataVencimeto) {
-		this.dataVencimeto = dataVencimeto;
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
 	}
 
 	public BigDecimal getValor() {
@@ -73,7 +73,7 @@ public class Titulo {
 	public void setStatus(StatusTitulo status) {
 		this.status = status;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
